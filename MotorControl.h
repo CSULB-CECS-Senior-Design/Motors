7371cb90129b1223721bf37473e0b11c2753d6cc
CECS-Jesus <jesus.perez04@student.csulb.edu>
2023-12-05 19:20:08 -0800

    Motor files

---
+++ MotorControl.h
@@ -0,0 +1,11 @@
+#ifndef MOTORCONTROL_H
+#define MOTORCONTROL_H
+
+void move_forward();
+void move_backwards();
+void turn_left();
+void turn_right();
+void pivot_left();
+void pivot_right();
+
+#endif //MOTORCONTROL_H
