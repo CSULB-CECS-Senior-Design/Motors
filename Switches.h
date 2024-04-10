0dfb3ca32ec7db91347037840434cf25ceb6c6ba
kesus <jesus.perez.cecs@gmail.com>
2024-04-10 14:57:32 -0700

    Added header files.

--- Switches.h
+++
@@ -1,9 +0,0 @@
-#ifndef Switches_H
-#define Switches_H
-
-#define SW1 0x10 //left switch
-#define SW2 0x01 //right switch
-#include "tm4c123gh6pm.h"
-void switch_init(void);
-
-#endif
---
+++ inc/Switches.h
@@ -0,0 +1,10 @@
+#ifndef SWITCHES_H
+#define SWITCHES_H
+
+#define SW1 0x10 //left switch
+#define SW2 0x01 //right switch
+#include "tm4c123gh6pm.h"
+
+void switch_init(void);
+
+#endif
