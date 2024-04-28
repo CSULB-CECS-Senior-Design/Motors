7ece0c10213da2835d807ce2daa873363945229e
kesus <jesus.perez.cecs@gmail.com>
2024-04-28 16:10:10 -0700

    Added objects.

--- Objects/motorsmain.d
+++ Objects/motorsmain.d
@@ -1,4 +1,4 @@
 ./objects/motorsmain.o: src\MotorsMain.c inc\tm4c123gh6pm.h \
   inc\MotorSetup.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
   inc\PLL.h inc\MotorControl.h inc\LED.h inc\Switches.h inc\SPI.h \
-  inc\Sensors.h inc\UART.h
+  inc\Sensors.h
