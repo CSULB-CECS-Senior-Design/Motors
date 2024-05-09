0bc40d5ab1738cb995d563a9af111fcd51712aff
Jesus Perez <119976553+CECS-Jesus@users.noreply.github.com>
2024-05-08 20:38:57 -0700

    Updated Object Files.

---
+++ Objects/sensors.d
@@ -0,0 +1,3 @@
+./objects/sensors.o: src\Sensors.c inc\tm4c123gh6pm.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h inc\Sensors.h \
+  inc\MotorSetup.h inc\PLL.h inc\MotorControl.h inc\LED.h inc\SPI.h
