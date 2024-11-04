3ba2cffb44eef56a41bfd37b725192c3e9f8a0f1
Jesus Perez <119976553+CECS-Jesus@users.noreply.github.com>
2024-11-03 18:19:20 -0800

    added finite state machine in place of the old switch statements for motor direction control.

--- src/MotorsMain.c
+++ src/MotorsMain.c
@@ -20,54 +20,84 @@ void system_init();
 void steering(uint16_t ahead_dist,uint16_t behind_dist);
 uint16_t receivedData;
 
+// Finite State Machine for motor States
+typedef enum {
+    STATE_STOP,
+    STATE_FORWARD,
+    STATE_BACKWARD,
+    STATE_TURN_LEFT,
+    STATE_TURN_RIGHT,
+    STATE_PIVOT_LEFT,
+    STATE_PIVOT_RIGHT
+} MotorState;
+
+// Array of function pointers for motor control functions located in MotorControl.C
+void (*stateActions[STATE_COUNT])(void) = {
+    stop_motors,      // STATE_STOP
+    move_forward,     // STATE_FORWARD
+    move_backward,    // STATE_BACKWARD
+    turn_left,        // STATE_TURN_LEFT
+    turn_right,       // STATE_TURN_RIGHT
+    pivot_left,       // STATE_PIVOT_LEFT
+    pivot_right       // STATE_PIVOT_RIGHT
+};
+
 int main(void) {
-	uint16_t ahead, behind;
-	DisableInterrupts();
-	system_init();
-	EnableInterrupts();
-	for(int x = 0; x < 10; x++){
-		ReadSensorsFIRFilter(&ahead, &behind);//sensor test
-	}
-		while(1) {
-			ReadSensorsFIRFilter(&ahead, &behind);	// Read sensor values
-			uint8_t frontDistance = convert_to_centimeter(ahead);
-			uint8_t backDistance = convert_to_centimeter(behind);
-			uint16_t transmitData = encode_distance(frontDistance, backDistance);
-			SPI_receive(&receivedData);
-			switch(receivedData){
-				case 'W':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					move_forward();
-					break;
-				case 'S':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					move_backward();
-					break;
-				case 'A':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					turn_left();
-					break;
-				case 'D':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					turn_right();
-					break;
-				case 'P':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					pivot_right();
-					break;
-				case 'O':
-					pwm_duty(LEFTPOWER, RIGHTPOWER);
-					pivot_left();
-					break;
-				case 'Q':
-					stop_motors();
-					break;
-				default:
-					stop_motors();
-					break;
-			}
-			SPI_transmit_word(transmitData);
-		}
+    uint16_t ahead, behind;
+    MotorState currentState = STATE_STOP; // Initial state
+
+    DisableInterrupts();
+    system_init();
+    EnableInterrupts();
+
+    // Initial sensor readings for filtering
+    for (int x = 0; x < 10; x++) {
+        ReadSensorsFIRFilter(&ahead, &behind); // Sensor test
+    }
+
+    while (1) {
+        ReadSensorsFIRFilter(&ahead, &behind);	// Read sensor values
+        uint8_t frontDistance = convert_to_centimeter(ahead);	//hold front IR Sensor Values
+        uint8_t backDistance = convert_to_centimeter(behind);	//hold back IR Sensor Values
+        uint16_t transmitData = encode_distance(frontDistance, backDistance);	// Encoded first byte to hold front IR Sensor Values and second byte to hold the back IR Sensor value
+
+        SPI_receive(&receivedData);  //wait to receive motor commands from pi 5 via SPI
+
+        // State motor logic depending on what command (unsigned char) is recieved from raspberry pi 5
+        switch (receivedData) {
+            case 'W':
+                currentState = STATE_FORWARD;
+                break;
+            case 'S':
+                currentState = STATE_BACKWARD;
+                break;
+            case 'A':
+                currentState = STATE_TURN_LEFT;
+                break;
+            case 'D':
+                currentState = STATE_TURN_RIGHT;
+                break;
+            case 'P':
+                currentState = STATE_PIVOT_RIGHT;
+                break;
+            case 'O':
+                currentState = STATE_PIVOT_LEFT;
+                break;
+            case 'Q':
+                currentState = STATE_STOP;
+                break;
+            default:
+                currentState = STATE_STOP;
+                break;
+        }
+
+        // Execute the motor direction for the current state
+        pwm_duty(LEFTPOWER, RIGHTPOWER);
+        stateActions[currentState]();
+
+	// Send both sensor values back
+        SPI_transmit_word(transmitData);
+    }
 }
 
 //System Initializations
